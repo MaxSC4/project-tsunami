9510727447fd6040566b72a59901056d163d4c28
courcellesmatthieu-star <courcellesmatthieu@gmail.com>
2025-10-16 19:27:44 +0200

    Ajout sur la carte du nom des villes

--- p1.py
+++ p1.py
@@ -38,22 +38,72 @@ extendex = [xmin, xmax, ymin, ymax]
 
 
 
-A=pd.read_csv('etopo5.grd',delimiter=' ',skiprows=6)
+A=pd.read_csv('etopo5.grd',delimiter=' ',skiprows=6)  #lecture du fichier de base
+
+positionville=pd.read_csv('data_villes.csv',delimiter=' ')
+positionville["Longitude_E"] = positionville["Longitude_E"] % 360
+
+
+#faire une boucle pour extraire les positions des villes
+
 
 
 masque=A<=0
-
-
-
 Bathymetrie=A[masque]
 
 
 
 Va=np.sqrt(-Bathymetrie*g)
 
-plt.plot
 plt.figure()
-plt.imshow(Va,'turbo',extent=extendex)
+
+plt.imshow(Va,extent=extendex)
+plt.plot(positionville["Longitude_E"], positionville["Latitude_N"], 'ro', markersize=8)
+for i, nom in enumerate(positionville["Ville/Port"]):
+    plt.text(positionville["Longitude_E"][i] + 1.5,
+             positionville["Latitude_N"][i] + 1.5,
+             nom, color='white', fontsize=8)
+
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.title("Carte avec les positions des villes")
+
+#%% Distance entre deux pointd 1 et 2 en lat/lon
+def Dist (lat1,lon1, lat2, lon2):
+    lat1=np.radians(lat1)
+    lon1=np.radians(lon1)
+    lat2=np.radians(lat2)
+    lon2=np.radians(lon2)
+    
+    
+    
+    R=6370000
+    d_lon=lon2-lon1
+    D=R*np.arccos(np.cos(lat1)*np.cos(lat2)*np.cos(d_lon)+np.sin(lat1)*np.sin(lat2))
+    return D
+    
+Dist(48,2,40,76)
+
+#%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
